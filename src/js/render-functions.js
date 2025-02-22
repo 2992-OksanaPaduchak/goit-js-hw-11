360cbd18d6e089b63ed2b498761cf100647346e7
Oksana Paduchak <2992paduhcak80@gmail.com>
2025-02-22 11:21:30 +0200

    homework 11

---
+++ src/js/render-functions.js
@@ -0,0 +1,32 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+function renderItemGallery(item) {
+    return `
+        <li class="gallery-item">
+            <a class="gallery-link" href="${item.largeImageURL}">
+                <img class="gallery-image"
+                src="${item.webformatURL}"
+                data-source="${item.largeImageURLl}"
+                alt="${item.tags}"/>
+            </a>
+            <div class="photo-info">
+                <span>likes: ${item.likes}</span>
+                <span>Views: ${item.views}</span>
+                <span>Comments: ${item.comments}</span>
+                <span>Downloads: ${item.downloads}</span>
+            </div>
+        </li>
+    `
+}
+
+export function renderGallery(listHits) {
+    document.querySelector('ul.gallery').innerHTML = listHits
+        .map(item => renderItemGallery(item))
+        .join('');
+    
+     new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',  
+        captionDelay: 250,
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,52 @@
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import "izitoast/dist/css/iziToast.min.css";
+
+import iconError from "./img/iconError.svg";
+import iziToast from "izitoast";
+import { getAllPhoto } from './js/pixabay-api'
+import { renderGallery } from './js/render-functions'
+
+
+const form = document.querySelector('form');
+const loader = document.querySelector('.loader__container');
+const galleryImage = document.querySelector('ul.gallery');
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const queryInput = evt.target.query.value.trim();
+    if (queryInput === "") {
+        return alert('Fill please field');
+    }
+
+    galleryImage.innerHTML = '';
+    loader.style.display = 'flex'
+
+    getAllPhoto(queryInput)
+        .then((res) => {
+            const hits = res.data.hits;
+
+            if (!hits.length) {
+                const message = `'Sorry, there are no images matching your search query. Please try again!'`;
+                iziToast.show({
+                    message,
+                    iconUrl: iconError,
+                    title: 'Erorr',
+                    titleColor: '#fff',
+                    titleSize: '16px',
+                    titleLineHeight: '24px',
+                    messageSize: '16px',
+                    messageLineHeight: '24px',
+                    messageColor: '#fff',
+                    backgroundColor: ' #ef4040',
+                    position: 'topRight',
+                })
+                return;
+            }
+
+            renderGallery(hits);
+            loader.style.display = 'none'
+        })
+
+    form.reset();
+});
+
+
