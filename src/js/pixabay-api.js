0b842910b258a9477dabbf8edf5e968d8e5662a4
Oksana Paduchak <2992paduhcak80@gmail.com>
2025-02-23 13:30:38 +0200

    task

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,37 +1,16 @@
 import axios from 'axios';
 
-// const instance = axios.create({
-//     baseURL: 'https://pixabay.com',
-//     params: {
-//         key: '48977888-b03943cb340f5b9503aa6f8a3',
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: 'true',
-//     }
-// });
-
-// export function getAllPhoto(query) {
-//     return instance.get('/api', {params: {q: query}});    
-// };
-
-
-const baseUrl = "https://pixabay.com";
-const endPoint = "/api";
-
-
-
-export function getAllPhoto(query) {
-    const params = new URLSearchParams({
+const instance = axios.create({
+    baseURL: 'https://pixabay.com',
+    params: {
         key: '48977888-b03943cb340f5b9503aa6f8a3',
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
-    });
-  
-    const url = baseUrl + endPoint + `?${params}`;
-    return axios.get(url, {params: {q: query}})
+    }
+});
 
+export function getAllPhoto(query) {
+    return instance.get('/api', {params: {q: query}});    
 };
-
-
  
